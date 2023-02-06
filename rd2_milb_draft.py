d05fcdb9ce5f6196580cc10caf2fed94410c6cf6
Jackson Wiegman <jdwiegman@gmail.com>
2023-02-06 21:39:36 +0000

    Now runs on Python3

--- rd2_milb_draft.py
+++ rd2_milb_draft.py
@@ -1,9 +1,10 @@
-import ConfigParser
+import configparser as ConfigParser
 import io
 import praw
 import time
 import json
 import requests
+import traceback
 
 POLL_TIME = 5 # PRAW has a model for polling, but Im too lazy to learn it
 
@@ -20,8 +21,8 @@ if __name__ == "__main__":
     with open("api-keys.ini", "r") as f:
         config_data = f.read()
 
-    config = ConfigParser.RawConfigParser(allow_no_value=True)
-    config.readfp(io.BytesIO(config_data))
+    config = ConfigParser.ConfigParser()
+    config.read('api-keys.ini')
 
     reddit_id = config.get("reddit", "reddit_id")
     reddit_secret = config.get("reddit", "reddit_secret")
@@ -39,12 +40,18 @@ if __name__ == "__main__":
 
 
     while True:
-        submission = reddit.submission(url = reddit_post)
-        for comment in submission.comments:
-            if comment.id not in old_comments:
-                slack_msg = comment.body + \
-                        '\n<http://reddit.com' + comment.permalink + '|Link>'
-                for keys, webhook in slack_webhook:
-                    send_slack_msg(slack_msg, webhook)
-                old_comments.append(comment.id)
-        time.sleep(POLL_TIME)
+        try:
+            submission = reddit.submission(url = reddit_post)
+            for comment in submission.comments:
+                if comment.id not in old_comments:
+                    slack_msg = comment.body + \
+                            '\n<http://reddit.com' + comment.permalink + '|Link>'
+                    for keys, webhook in slack_webhook:
+                        send_slack_msg(slack_msg, webhook)
+                    old_comments.append(comment.id)
+            time.sleep(POLL_TIME)
+        #except qpraw.prawcore.exceptions.RequestException:
+        except:
+            print("Reddit timeout most likely")
+            traceback.print_exc()
+            time.sleep(10)
